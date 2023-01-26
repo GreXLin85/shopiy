4ef1fb15e6bcac77878d293e384f634e9b824369
GreXLin85 <grexlin85@gmail.com>
2023-01-26 21:11:23 +0200

    bug fix

--- src/components/pageContainer.tsx
+++ src/components/pageContainer.tsx
@@ -4,9 +4,10 @@ import styled from 'styled-components';
 const PageContainer = styled.div`
     width: 80vw;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
     margin: 0px auto;
+    gap: 2rem;
 `;
 
 export default function pageContainer({children}: {
