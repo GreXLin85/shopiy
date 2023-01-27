b437bd90276ff40184200e8cb2b0071d7408c8fc
GreXLin85 <grexlin85@gmail.com>
2023-01-27 07:48:11 +0200

    Title added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import styled from 'styled-components';
 import './App.css';
 import NavigationBar from './components/navigationBar';
 import PageContainer from './components/pageContainer';
 import Products from './components/products';
 import ProductSlider from './components/productSlider';
 
+const ProductsTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+`;
 
 
 function App() {
@@ -13,6 +19,7 @@ function App() {
       <PageContainer>
         <ProductSlider></ProductSlider>
         <Products></Products>
+        <ProductsTitle>All Products</ProductsTitle>
       </PageContainer>
 
     </div>
